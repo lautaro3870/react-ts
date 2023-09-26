3a50b2cc488c1ce154672e327e23892d09de8819
DI SBROIAVACCA Lautaro Nicolas <ln.disbroiavacca@soprasteria.com>
2023-09-26 16:47:06 +0200

    Más campos en el form

--- src/componentes/Formularios.tsx
+++ src/componentes/Formularios.tsx
@@ -1,9 +1,16 @@
 import { useForm } from "../hooks/useForm";
 
+enum Sexos {
+    Masculino = "M",
+    Femenino = "F"
+}
+
 export const Formularios = () => {
-  const { formulario, onChange, email, password } = useForm({
+  const { formulario, onChange, email, password, edad } = useForm({
     email: "lau@gmail.com",
     password: "1234",
+    edad: 22,
+    sexo: Sexos.Femenino,
   });
 
   return (
@@ -23,6 +30,23 @@ export const Formularios = () => {
         className="form-control mt-2 mb-2"
         onChange={(e) => onChange(e.target.value, "password")}
       />
+      <input
+        type="number"
+        placeholder="Edad"
+        value={edad}
+        className="form-control mt-2 mb-2"
+        onChange={(e) => onChange(e.target.value, "edad")}
+      />
+      <select
+        name=""
+        onChange={(e) => onChange(e.target.value, "sexo")}
+        className="form-control mt-2 mb-2"
+        id=""
+        value={formulario.sexo}
+      >
+        <option value={Sexos.Masculino}>Masculino</option>
+        <option value={Sexos.Femenino}>Femenino</option>
+      </select>
       <code>
         <pre>{JSON.stringify(formulario, null, 2)}</pre>
       </code>
